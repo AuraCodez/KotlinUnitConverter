faa0218b8229251f1f29cc0dfe7d750fdeb261e4
Ryan L <fartman553@gmail.com>
2023-01-12 22:36:45 -0500

    Constructed the UI, added API
    Implemented some dependencies in build.gradle
    Used Retrofit,  type-safe HTTP client for Android and Java. to allows to make network requests from the API

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.exchangeratesapi.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
     }
 }
