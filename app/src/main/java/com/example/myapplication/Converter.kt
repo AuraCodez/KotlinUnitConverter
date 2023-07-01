0d09db2acfb4097a4c742e8982d27e92c3df397b
Ryan <86922669+AuraCodez@users.noreply.github.com>
2023-07-01 14:44:10 -0400

    Fixed some crashes and resolved some API key issues

--- app/src/main/java/com/example/myapplication/Converter.kt
+++ app/src/main/java/com/example/myapplication/Converter.kt
@@ -1,29 +1,72 @@
 package com.example.myapplication
-
+import RetrofitClient
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.jaredrummler.materialspinner.MaterialSpinner
+import com.jaredrummler.materialspinner.MaterialSpinnerAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class Converter : AppCompatActivity() {
+
+    private val api: ApiService = RetrofitClient.api
+    private lateinit var fromSpinner: MaterialSpinner
+    private lateinit var toSpinner: MaterialSpinner
+    private lateinit var amountEditText: EditText
+    private lateinit var convertButton: Button
+    private lateinit var resultEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.currency_page)
 
-        val fromSpinner = findViewById<MaterialSpinner>(R.id.spinner);
-        val toSpinner = findViewById<MaterialSpinner>(R.id.spinner2);
-        val amountEditText = findViewById<EditText>(R.id.enterCurrency);
-        val convertButton = findViewById<Button>(R.id.convertButton);
-        val resultEditText = findViewById<EditText>(R.id.etSecondCurrency);
+        fromSpinner = findViewById(R.id.fromSpinner)
+        toSpinner = findViewById(R.id.toSpinner)
+        amountEditText = findViewById(R.id.enterCurrency)
+        convertButton = findViewById(R.id.convertButton)
+        resultEditText = findViewById(R.id.etSecondCurrency)
 
+        val apiKey = intent.getStringExtra("API_KEY") ?: ""
+        Log.d("ConverterActivity", "API key: $apiKey")
+        fetchCurrencySymbols(apiKey)
 
+    }
 
+    private fun populateSpinners(symbols: List<String>, spinner: MaterialSpinner) {
+        when(spinner.id) {
+            R.id.toSpinner -> {
+                val adapter = MaterialSpinnerAdapter(spinner.context, symbols)
+                spinner.setAdapter(adapter)
 
+            }
+            R.id.fromSpinner -> {
+                val adapter = MaterialSpinnerAdapter(spinner.context, symbols)
+                spinner.setAdapter(adapter)
+            }
+        }
+    }
+    private fun fetchCurrencySymbols(apiKey: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = api.getCurrencySymbols(apiKey)
+                if (response.isSuccessful) {
+                    // Get the response body and perform actions on it only if it's not null
+                    response.body()?. let {data ->
+                        // Convert the map values to a list
+                        val symbols = data.symbols.values.toList()
+                        Log.d("Converter", "Currency Symbols: $symbols")
+                        populateSpinners(symbols, toSpinner)
+                        populateSpinners(symbols, fromSpinner)
+                    }
+                }
+            } catch(e: Exception) {
+                e.printStackTrace()
+            }
 
-
-
-
+        }
 
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,8 @@
+package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.Converter
-import com.example.myapplication.MorseCodeConverter
-import com.example.myapplication.R
-import com.example.myapplication.UnitConverter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +12,12 @@ class MainActivity : AppCompatActivity() {
         val currencyBtn = findViewById<Button>(R.id.currencyButton)
         currencyBtn.setOnClickListener {
             val intent = Intent(this, Converter::class.java)
+            val inputStream = assets.open("apiKey.txt")
+            val apiKey = inputStream.bufferedReader().use {
+                it.readText()
+            }
+
+            intent.putExtra("API_KEY", apiKey)
             startActivity(intent)
         }
 
@@ -30,11 +33,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val inputStream = assets.open("apiKey.txt")
-        val apiKey = inputStream.bufferedReader().use {
-            it.readText()
-        }
-
-        // The api instance could be used here to call the API methods
     }
-}
+ }
